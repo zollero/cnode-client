97dc74f2ab0d6daa4287c2ac06b143dae912b4c6
zhanghua54 <zhanghua54@wanda.cn>
2017-08-11 22:27:47 +0800

    测试接口调用

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default class App extends React.Component {
 
@@ -29,14 +29,16 @@ export default class App extends React.Component {
 
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        {
-          data.map((v, i) => {
-            return <Text key={ i }>{ v.title }</Text>
-          })
-        }
+          <ScrollView>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          {
+            data.map((v, i) => {
+              return <Text key={ i }>{ v.title }</Text>
+            })
+          }
+        </ScrollView>
       </View>
     );
   }
